2e71afb691caee5c8901d9ee069136aade7043a9
aepatino5112 <aepatino5112@gmail.com>
2026-04-04 20:36:47 -0400

    chore: add color & font system to the globals.css

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const manropeSans = Manrope({
+  variable: "--font-sans",
   subsets: ["latin"],
 });
 
@@ -25,9 +20,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${manropeSans.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }

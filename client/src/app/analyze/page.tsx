4f9e751fdcaffc4e8445177a443ae75bdf033b34
aepatino5112 <aepatino5112@gmail.com>
2026-04-05 19:39:39 -0400

    feat: add heading & description of the analyze page

--- client/src/app/analyze/page.tsx
+++ client/src/app/analyze/page.tsx
@@ -4,7 +4,29 @@ const Analyze = () => {
 
     return (
         <main>
-            <ThemeToggle />
+            {/* Main Heading & description */}
+            <h4 className="pb-4 mt-8 md:mt-0">Analyze Your Job Fit</h4>
+            <div className="max-w-4xl text-left">
+                <p className="leading-normal">Upload your resume and paste the job description to discover your exact skill gaps.</p>
+                <p className="leading-normal my-2 md:my-0">Takes less than 15 seconds.</p>
+            </div>
+
+            {/* Main wrapper */}
+            <div className="my-8">
+
+                {/* Inputs container */}
+                <div>
+
+                    {/* Input for resume */}
+
+                    {/* Input for job description */}
+
+                </div>
+
+                {/* CTA button */}
+
+                {/* Disclaimer about resume storage */}
+            </div>
         </main>
     );
 };
